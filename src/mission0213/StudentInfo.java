452005803049ca3e26716cb57aebc80ddc13dc52
JH <jiho4890@gmail.com>
2026-02-13 15:10:17 +0900

    feat: 2026.02.13 과제 추가

--- src/mission0213/StudentInfo.java
+++ src/mission0213/StudentInfo.java
@@ -1,4 +1,18 @@
 package mission0213;
 
 public class StudentInfo {
+    public static void main(String[] args) {
+        String name = "지호";
+        int age = 22; //만 나이
+        double height = 180.1;
+        char grade = '4';
+        boolean isStudent = false;
+
+        System.out.println("=== 학생 정보 ===");
+        System.out.println("이름: " + name);
+        System.out.println("나이: " + age + "세");
+        System.out.println("키: " + height + "cm");
+        System.out.println("학년: " + grade + "학년");
+        System.out.println("재학중: " + isStudent);
+    }
 }
