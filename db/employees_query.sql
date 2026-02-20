50f19b0e482642b01d2a5ccf0a798fc7908fe9a3
JH <126761060+vanilalatte03@users.noreply.github.com>
2026-02-20 14:35:47 +0900

    feat: 2~4번 문제 추가

--- db/employees_query.sql
+++ db/employees_query.sql
@@ -1,2 +1,17 @@
+--1
 SELECT name, position
 FROM sparta_employees;
+
+--2
+select distinct position
+from sparta_employees;
+
+--3
+select *
+from sparta_employees
+where salary between 40000 and 60000;
+
+--4
+select *
+from sparta_employees
+where hire_date < '2023-01-01';
