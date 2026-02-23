6d25bd8ff1b7313ad447270611c567337bd9fc42
JH <jiho4890@gmail.com>
2026-02-23 14:36:44 +0900

    feat: 반복문 퀘스트 3

---
+++ src/mission0223/Q3.java
@@ -0,0 +1,11 @@
+package mission0223;
+
+public class Q3 {
+    public static void main(String[] args) {
+        for(int i = 2; i <= 9; i++){
+            for(int j = 1; j <= 9; j++){
+                System.out.println(i + " x " + j +" = " + i*j);
+            }
+        }
+    }
+}
