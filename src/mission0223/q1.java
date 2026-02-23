15abfad3c44e48ca53d8b3f264721ef964719cd8
JH <jiho4890@gmail.com>
2026-02-23 14:22:04 +0900

    feat: 반복문 퀘스트 1

---
+++ src/mission0223/q1.java
@@ -0,0 +1,9 @@
+package mission0223;
+
+public class q1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
+}
