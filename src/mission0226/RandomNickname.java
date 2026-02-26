56a4671c764d1e1e793aba5a0b917a04f17669d4
JH <jiho4890@gmail.com>
2026-02-26 14:31:03 +0900

    feat: 랜덤 닉네임 생성기

--- src/mission0226/RandomNickname.java
+++ src/mission0226/RandomNickname.java
@@ -1,7 +1,6 @@
 package mission0226;
 
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class RandomNickname {
     private static final String[] Word1 = {"기절초풍", "멋있는", "재미있는"};
