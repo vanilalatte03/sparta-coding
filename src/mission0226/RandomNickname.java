38c5fb2a7c956578fe883233a42f7e0e1a0338d2
JH <jiho4890@gmail.com>
2026-02-26 14:30:34 +0900

    feat: 랜덤 닉네임 생성기

--- src/mission0226/RandomNickname.java
+++ src/mission0226/RandomNickname.java
@@ -4,9 +4,9 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class RandomNickname {
-    private static final String[] WORD1 = {"기절초풍", "멋있는", "재미있는"};
-    private static final String[] WORD2 = {"도전적인", "노란색의", "바보같은"};
-    private static final String[] WORD3 = {"돌고래", "개발자", "오랑우탄"};
+    private static final String[] Word1 = {"기절초풍", "멋있는", "재미있는"};
+    private static final String[] Word2 = {"도전적인", "노란색의", "바보같은"};
+    private static final String[] Word3 = {"돌고래", "개발자", "오랑우탄"};
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -18,13 +18,13 @@ public class RandomNickname {
     public String createRandomNickname(String name){
         Random random = new Random();
 
-        int firstWord = random.nextInt(WORD1.length);
-        int secondWord = random.nextInt(WORD2.length);
-        int thirdWord = random.nextInt(WORD3.length);
+        int firstWord = random.nextInt(Word1.length);
+        int secondWord = random.nextInt(Word2.length);
+        int thirdWord = random.nextInt(Word3.length);
 
-        String nickName = WORD1[firstWord] + " "
-                + WORD2[secondWord] + " "
-                + WORD3[thirdWord] + " " +
+        String nickName = Word1[firstWord] + " "
+                + Word2[secondWord] + " "
+                + Word3[thirdWord] + " " +
                 name;
 
         return nickName ;
